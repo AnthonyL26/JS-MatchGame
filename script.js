07101b2120b6172a0b087ce698f10262c43ae8d8
Your Name <you@example.com>
2023-10-26 10:55:32 -0400

    3

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 class Card {
-    constructor(color) {
-        this.color = color;
+    constructor(color, color2, id) {
+        this.backColor = color;
+        this.frontColor = color2;
+        this.id = id;
     }
-    getElement() {
+    createElement() {
         var element = document.createElement("div");
-        element.style.backgroundColor = this.color;
-        element.innerHTML = "‎ R";
-        element.onclick = this.switchColor();
+        element.style.backgroundColor = this.backColor;
+        element.innerHTML = "‎";
+        element.class = "Card";
+        element.id = this.id;
+        element.setAttribute("onclick", "switchColor(this)");
         return element;
     }
-    switchColor() {
-        this.getElement.style.backgroundColor = "blue";
+}
+
+var colors = ["red", "red", "blue", "blue", "yellow", "yellow", "green", "green", "orange", "orange", "purple", "purple", "cyan", "cyan", "pink", "pink"];
+
+function switchColor(card) {
+    console.log(1);
+    var cardId = card.getAttribute("id")
+     if (card.style.backgroundColor != colors[cardId]) {
+        console.log(2);
+        card.style.backgroundColor = colors[cardId];
+    } else {
+        card.style.backgroundColor = "white";
     }
 }
+
 
 
 // Color Pairs: red, blue, yellow, green, Orange, purple, Cyan, pink
 
 function initialize() {
+    gameDescription = document.getElementById("info");
+    document.getElementById("info").style.display = "none";
+    
+
     for (let i = 0; i < 16; i++) {
-        var newCard = new Card("red");
-        document.getElementById("container").append(newCard.getElement());
+        var newCard = new Card("white", colors[i], i);
+        document.getElementById("container").append(newCard.createElement());
     }
 }
+
+function toggleText(){
+    
+}
+
 
 
 
